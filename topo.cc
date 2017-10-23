46fbd036ab16677ee0d3a4faa7fd6b27bdf1cc8c
Harsh1-1 <harsh16023@iiitd.ac.in>
2017-10-24 03:49:06 +0530

    update

--- topo.cc
+++ topo.cc
@@ -42,7 +42,7 @@ int
 main (int argc, char *argv[])
 {
   bool verbose = true;
-  uint32_t nCsma = 1;
+  uint32_t nCsma = 2;
   uint32_t nWifi = 3;
   bool tracing = false;
 
@@ -69,18 +69,19 @@ main (int argc, char *argv[])
       LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
     }
 
-  NodeContainer p2pNodes;
-  p2pNodes.Create (2);
+  // NodeContainer p2pNodes;
+  // p2pNodes.Create (2);
+  //
+  // PointToPointHelper pointToPoint;
+  // pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  // pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  // NetDeviceContainer p2pDevices;
+  // p2pDevices = pointToPoint.Install (p2pNodes);
 
-  NetDeviceContainer p2pDevices;
-  p2pDevices = pointToPoint.Install (p2pNodes);
 
   NodeContainer csmaNodes;
-  csmaNodes.Add (p2pNodes.Get (1));
+  // csmaNodes.Add (p2pNodes.Get (1));
   csmaNodes.Create (nCsma);
 
   CsmaHelper csma;
@@ -90,9 +91,12 @@ main (int argc, char *argv[])
   NetDeviceContainer csmaDevices;
   csmaDevices = csma.Install (csmaNodes);
 
+
+
+
   NodeContainer wifiStaNodes;
   wifiStaNodes.Create (nWifi);
-  NodeContainer wifiApNode = p2pNodes.Get (0);
+  NodeContainer wifiApNode = csmaNodes.Get (0);
 
   YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
   YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
@@ -116,6 +120,8 @@ main (int argc, char *argv[])
   NetDeviceContainer apDevices;
   apDevices = wifi.Install (phy, mac, wifiApNode);
 
+
+
   MobilityHelper mobility;
 
   mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
@@ -130,19 +136,21 @@ main (int argc, char *argv[])
                              "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
   mobility.Install (wifiStaNodes);
 
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.Install (wifiApNode);
+  // mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  // mobility.Install (wifiApNode);
 
   InternetStackHelper stack;
   stack.Install (csmaNodes);
-  stack.Install (wifiApNode);
+  // stack.Install (wifiApNode);
   stack.Install (wifiStaNodes);
 
   Ipv4AddressHelper address;
 
-  address.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer p2pInterfaces;
-  p2pInterfaces = address.Assign (p2pDevices);
+  // address.SetBase ("10.1.1.0", "255.255.255.0");
+  // Ipv4InterfaceContainer p2pInterfaces;
+  // p2pInterfaces = address.Assign (p2pDevices);
+
+
 
   address.SetBase ("10.1.2.0", "255.255.255.0");
   Ipv4InterfaceContainer csmaInterfaces;
@@ -150,7 +158,9 @@ main (int argc, char *argv[])
 
   address.SetBase ("10.1.3.0", "255.255.255.0");
   address.Assign (staDevices);
-  address.Assign (apDevices);
+  Ipv4InterfaceContainer apsecondInterface;
+  apsecondInterface = address.Assign (apDevices);
+
 
   UdpEchoServerHelper echoServer (9);
 
@@ -172,12 +182,12 @@ main (int argc, char *argv[])
 
   Simulator::Stop (Seconds (10.0));
 
-  if (tracing == true)
-    {
-      pointToPoint.EnablePcapAll ("third");
-      phy.EnablePcap ("third", apDevices.Get (0));
-      csma.EnablePcap ("third", csmaDevices.Get (0), true);
-    }
+  // if (tracing == true)
+  //   {
+  //     // pointToPoint.EnablePcapAll ("third");
+  //     phy.EnablePcap ("third", apDevices.Get (0));
+  //     csma.EnablePcap ("third", csmaDevices.Get (0), true);
+  //   }
 
   Simulator::Run ();
   Simulator::Destroy ();
